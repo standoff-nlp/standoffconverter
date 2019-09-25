ba6ae1959066c9e235ad92956a29ce9890bafcc0
David Lassner <davidlassner@gmail.com>
2019-09-25 17:22:39 +0200

    migrated code to dhspacy

--- standoffconverter/__init__.py
+++ standoffconverter/__init__.py
@@ -159,37 +159,4 @@ class Standoff:
                 and attrs_equal(attribute, standoff["attrib"])):
                 return True
         return False
-            
-    def add_spacy_annotations(self, doc, inds, labels, attributes, depths=None, unique=True):
-        """add a standoff annotations from a spacy document.
-
-        In order to add annotations from a spacy document, the annotations need to be 
-        converted from token-level annotations to character-level annotations
-
-        arguments:
-        doc (spacy.tokens.Doc) -- A Document that was created from self.plain
-        inds (list) -- the list of *token* indices
-        labels (list) -- the list of labels to be used as XML tags
-        attributes (list) -- the list of attributes (dicts) to be used as XML attributes
-
-        keyword arguments:
-        depths (list) -- list of depths for each attribute. The depth specifies the order 
-                         of XML tags that have the same beginning and ending index.
-                         for the same begin and end, a lower depth annotation includes 
-                         a higher depth annotation.
-        unique (bool) -- whether to allow for duplicate annotations
-        """
-
-        if depths is None:
-            depths =  [None]*len(inds)
         
-        assert (doc.text == self.plain), "spacy document does not fit the self.plain text."
-
-        assert len(inds) == len(labels) == len(attributes) == len(depths), "new standoff\
-         params have to have all same lengths."
-
-        tokeni2idx = {t.i:(t.idx,t.idx+tchar) for t in doc for tchar in range(len(t.text)+1)} 
-
-        for ind, label, attribute, depth in zip(inds, labels, attributes, depths):
-            begin, end = tokeni2idx[ind]
-            self.add_annotation(begin, end, label, depth, attribute, unique)
--- standoffconverter/tests/test_standoffconverter.py
+++ standoffconverter/tests/test_standoffconverter.py
@@ -42,27 +42,6 @@ class TestStandoffConverter(unittest.TestCase):
             so.is_duplicate_annotation(0,len(so.plain), "W", {})
         )
 
-    # def test_add_spacy_annotations(self):
-        
-    #     tree = etree.fromstring(input_xml2)
-    #     so = standoffconverter.Standoff()
-    #     so.from_lxml_tree(tree)
-
-    #     spacified = nlp(so.plain)
-    #     inds = []
-    #     labels = []
-    #     attributes = []
-    #     for itoken, token in enumerate(spacified):
-    #         if token.is_digit:
-    #             inds.append(itoken)
-    #             labels.append("digit")
-    #             attributes.append({"resp":"spacy"})
-
-    #     so.add_spacy_annotations(spacified, inds, labels, attributes)
-        
-    #     output_xml = so.to_xml()
-    #     expected_output = "<W><text type='a'>The answer is <digit resp='spacy'>42</digit>.</text></W>"
-    #     self.assertTrue(expected_output == output_xml)
 
 if __name__ == '__main__':
     unittest.main()
