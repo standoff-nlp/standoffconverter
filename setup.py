7c8fb845647fb5e023ac1d225899edc845a139c6
David Lassner <davidlassner@gmail.com>
2019-12-18 22:14:02 +0100

    fixed url

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='standoffconverter',
-      version='0.6',
+      version='0.6.1',
       description='converter from xml to standoff and back',
-      url='https://https://github.com/standoff-nlp/standoffconverter',
+      url='https://github.com/standoff-nlp/standoffconverter',
       author='David Lassner',
       author_email='lassner@tu-berlin.de',
       license='MIT',
