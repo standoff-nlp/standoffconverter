6d6af04bd404d619af61b3039dee9ff69b6e208a
Julius <j.coburger@campus.tu-berlin.de>
2021-03-10 17:37:46 +0100

    add span feature

--- standoffconverter/tests/test_standoffconverter.py
+++ standoffconverter/tests/test_standoffconverter.py
@@ -3,7 +3,7 @@ import os
 from lxml import etree
 import numpy as np
 
-import standoffconverter
+import standoffs as standoffconverter
 
 input_xml1 = b'''<TEI><teiHeader></teiHeader><text><body><p>1 2 3 4 5 6 7 9 10</p><p> 11<lb/> 12 13 14</p></body></text></TEI>'''
 
@@ -272,6 +272,53 @@ class TestStandoffConverter(unittest.TestCase):
 
         expected_output = "<text><body><p>1 2 3 4 5 6 7 9 10</p><p> 11<lb/> 12 13 14</p></body></text>"
 
+        self.assertTrue(
+            output_xml == expected_output
+        )
+        
+        
+    def test_span_1(self):
+        tree = etree.fromstring(input_xml1)
+        so = standoffconverter.Converter(tree)
+        so.ensure_cache()
+    
+        so.add_span(
+            id="test1",
+            begin=2,
+            end=7, 
+            tag="tag1",
+            depth=None
+            )
+        
+        output_xml = etree.tostring(so.text_el).decode("utf-8")
+        print("\n",output_xml)
+        expected_output = "<text><body><p>1 <tag1Span spanTo=\"test1\"/>2 3 4<anchor xml_id=\"test1\"/> 5 6 7 9 10</p><p> 11<lb/> 12 13 14</p></body></text>"
+        print("\n", expected_output)
+
+
+        self.assertTrue(
+            output_xml == expected_output
+        )
+
+        
+    def test_span_2(self):
+        tree = etree.fromstring(input_xml1)
+        so = standoffconverter.Converter(tree)
+        so.ensure_cache()
+    
+        so.add_span(
+            id="test2",
+            begin=2,
+            end=22, 
+            tag="tag2",
+            depth=None
+            )
+        
+        output_xml = etree.tostring(so.text_el).decode("utf-8")
+        print("\n", output_xml)
+
+        expected_output = "<text><body><p>1 <tag2Span spanTo=\"test2\"/>2 3 4 5 6 7 9 10</p><p> 11<lb/> <anchor xml_id=\"test2\"/>12 13 14</p></body></text>"
+        print("\n", expected_output)
         self.assertTrue(
             output_xml == expected_output
         )
