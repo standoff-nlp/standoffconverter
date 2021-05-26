c39d22ad5161ad2d48b3f0c18fe0210c7d9331d2
David Lassner <davidlassner@gmail.com>
2021-05-26 14:46:08 +0200

    improve markup and ids to sentence tags

--- examples/wysiwyg.py
+++ examples/wysiwyg.py
@@ -8,11 +8,7 @@ st.set_page_config(layout="wide")
 st.write("# Interactive demo of the standoff converter")
 
 st.write('''
-This demo shows the two steps involved to get from TEI XML to plain text. This way you can use standard NLP tools on your TEI documents. 
-
-1. On the left side, you can modify the TEI document
-2. In the center, the standoff view of the document is shown, where the text and the annotations are separated
-3. On the right, a view of the document is created that (as an example) reduces the whitespace between tags and adds line breaks for `<lb/>` tags.  
+This demo shows the steps involved to get from TEI XML to plain text, identify sentences and put the found sentences as `<s>` into the TEI. It illustrates how you can use standard NLP tools on your TEI documents. You can modify the input document to see how things change!
 
 [The code for this demo is available in our Github Repository](https://github.com/standoff-nlp/standoffconverter/blob/master/examples/wysiwyg.py)  
 
@@ -43,7 +39,7 @@ input_xml = col1.text_area(
 
 
 
-col2.text("collapsed table")
+col2.write("1. Standoff representation")
 
 col2.code(
     """# 1. create standoff 
@@ -55,7 +51,7 @@ tree = etree.fromstring(input_xml)
 so = Standoff(tree)
 
 col2.write(so.collapsed_table)
-col3.text("plain text")
+col3.write("2. Plain text view")
 
 view = (
     View(so.table)
@@ -90,7 +86,7 @@ st.write("## Apply spacy sentencizer and add `<s>`-tags")
 
 
 col1, col2, col3 = st.beta_columns(3)
-col1.write('spacy sentencizer')
+col1.write('3. Apply spacy sentencizer')
 col1.code(
     """# 3. annotate with NLP 
 # to split sentences 
@@ -100,38 +96,40 @@ nlp.add_pipe('sentencizer')
 
 sentences = []
 for sent in nlp(plain).sents:
-col1.write(f"* {sent}")
-sentences.append(sent)
+    col1.write(f"* {sent}")
+    sentences.append(sent)
 """)
 
 from spacy.lang.en import English
 nlp = English()
 nlp.add_pipe('sentencizer')
 
+col1.write('spacy found the following sentences:')
 sentences = []
 for sent in nlp(plain).sents:
     col1.write(f"* {sent}")
     sentences.append(sent)
 
-col2.write('add annotations')
+col2.write('4. Add annotations')
 col2.code(
     """# 4. retrieve results from spacy,
 # resolve original character positions
 # and add annotations to the tree
-for sent in sentences:
+for isent, sent in enumerate(sentences):
 
-start_ind = lookup.get_pos(sent.start_char)
-end_ind = lookup.get_pos(sent.end_char-1)+1
+    start_ind = lookup.get_pos(sent.start_char)
+    end_ind = lookup.get_pos(sent.end_char-1)+1
 
-so.add_inline(
-    begin=start_ind,
-    end=end_ind,
-    tag="s",
-    depth=None
-)
+    so.add_inline(
+        begin=start_ind,
+        end=end_ind,
+        tag="s",
+        depth=None,
+        attrib={'id':f'{isent}'}
+    )
 
 """)
-for sent in sentences:
+for isent, sent in enumerate(sentences):
 
     start_ind = lookup.get_pos(sent.start_char)
     end_ind = lookup.get_pos(sent.end_char-1)+1
@@ -141,7 +139,8 @@ for sent in sentences:
             begin=start_ind,
             end=end_ind,
             tag="s",
-            depth=None
+            depth=None,
+            attrib={'id':f'{isent}'}
         )
     except (ValueError, IndexError):
         raise ValueError(f"Unable to add sentence tag for '{sent}'. It probably violates the tree constraint of XML.")
