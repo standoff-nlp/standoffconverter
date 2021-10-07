ae6ac1359da35c99483f5fe99873bfb2a59e936b
David Lassner <davidlassner@gmail.com>
2021-10-07 16:39:34 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='standoffconverter',
-      version='0.8.7',
+      version='0.8.8',
       description='converter from xml to standoff and back',
       url='https://github.com/standoff-nlp/standoffconverter',
       author='David Lassner',
